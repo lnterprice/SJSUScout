c90c9c19e2bea946c7f582a4f1710c87e28d99f7
lnterprice <sphi0706@gmail.com>
2025-02-09 16:58:10 -0800

    Replaced main method with Summoner class and created new methods in Champion class

--- Champion.py
+++ Champion.py
@@ -4,17 +4,17 @@ class ChampionMain:
         if type(unparsedMatchData) == list:
             self.championID = unparsedMatchData[0]['championId']
             self.uData = unparsedMatchData
-            self.info = {self.championID: {}}
             self.player = unparsedMatchData[0]['riotIdGameName'] + "#" + unparsedMatchData[0]['riotIdTagline']
             self.championName = unparsedMatchData[0]['championName']
+            self.info = {(self.championName, self.championID): {}}
             self.mastery = unparsedMatchData[0]['champExperience']
 
         elif type(unparsedMatchData) == dict:
             self.championID = unparsedMatchData['championId']
             self.uData = [unparsedMatchData]
-            self.info = {self.championID: {}}
             self.player = unparsedMatchData['riotIdGameName'] + "#" + unparsedMatchData['riotIdTagline']
             self.championName = unparsedMatchData['championName']
+            self.info = {(self.championName, self.championID): {}}
             self.mastery = unparsedMatchData['champExperience']
         else:
             raise ValueError("[-] Unparsed Match Data neither List or String type")
@@ -38,6 +38,12 @@ class ChampionMain:
         self.getAverageKDA()
         self.getAvgWinRate()
         self.getAvgCSMin()
+        self.getGPM()
+        self.getAvgKP()
+        self.getAvgDPM()
+        self.getAvgAbilityUsage()
+        self.getSkillShotsDodged()
+        self.getAverageVisionScore()
 
     # statistics cranker
 
@@ -49,8 +55,8 @@ class ChampionMain:
             kills += stat['kills']
             deaths += stat['deaths']
             assists += stat['assists']
-        self.info[self.championID]['KDA'] = (kills/len(self.uData), deaths/len(self.uData), assists/len(self.uData))
-        return self.info[self.championID]['KDA']
+        self.info[(self.championName, self.championID)]['KDA'] = (kills/len(self.uData), deaths/len(self.uData), assists/len(self.uData))
+        return self.info[(self.championName, self.championID)]['KDA']
 
     def getAvgWinRate(self):
         totalGames = 0
@@ -60,25 +66,68 @@ class ChampionMain:
                 wins += 1
             totalGames += 1
         try:
-            self.info[self.championID]['Winrate'] = wins/   totalGames
+            self.info[(self.championName, self.championID)]['Winrate'] = wins/   totalGames
         except ZeroDivisionError:
-            self.info[self.championID]['Winrate'] = 0
-        return self.info[self.championID]['Winrate']
+            self.info[(self.championName, self.championID)]['Winrate'] = 0
+        return self.info[(self.championName, self.championID)]['Winrate']
     
+
     def getAvgCSMin(self):
         totalCSMin = 0
         for stat in self.uData:
             totalCSMin += (stat['totalMinionsKilled']/(stat['challenges']['gameLength']/60))
-        self.info[self.championID]['AvgCS'] = totalCSMin/len(self.uData)
-        return self.info[self.championID]['AvgCS']
+        self.info[(self.championName, self.championID)]['AvgCS'] = totalCSMin/len(self.uData)
+        return self.info[(self.championName, self.championID)]['AvgCS']
+
+    def getGPM(self):
+        totalGold = 0
+        for stat in self.uData:
+            totalGold += stat['challenges']['goldPerMinute']
+        self.info[(self.championName, self.championID)]['GPM'] = totalGold/len(self.uData)
+        return self.info[(self.championName, self.championID)]['GPM']
+
+    def getAvgKP(self):
+        totalKP = 0
+        for stat in self.uData:
+            totalKP += stat['challenges']['killParticipation']
+        self.info[(self.championName, self.championID)]['KP'] = totalKP/len(self.uData)
+        return self.info[(self.championName, self.championID)]['KP']
+    
+    def getAvgDPM(self):
+        totalDPM = 0
+        for stat in self.uData:
+            totalDPM += stat['challenges']['damagePerMinute']
+        self.info[(self.championName, self.championID)]['AvgDPM'] = totalDPM/len(self.uData)
+        return self.info[(self.championName, self.championID)]['AvgDPM']
+    
+    def getAvgAbilityUsage(self):
+        totalAbilityUsage = 0
+        for stat in self.uData:
+            totalAbilityUsage += stat['challenges']['abilityUses']
+        self.info[(self.championName, self.championID)]['AvgAbUSage'] = totalAbilityUsage/len(self.uData)
+        return self.info[(self.championName, self.championID)]['AvgAbUSage']
+    
+    def getSkillShotsDodged(self):
+        totalSkillShotsRatio = 0
+        for stat in self.uData:
+            totalSkillShotsRatio += (stat['challenges']['skillshotsDodged']/(stat['challenges']['skillshotsDodged'] + stat['challenges']['skillshotsHit']))
+        self.info[(self.championName, self.championID)]['SkillShotRatio'] = totalSkillShotsRatio/len(self.uData)
+        return self.info[(self.championName, self.championID)]['SkillShotRatio']
+    
+    def getAverageVisionScore(self):
+        vScore = 0
+        for stat in self.uData:
+            vScore += stat['visionScore']
+        self.info[(self.championName, self.championID)]['VisionScore'] = vScore/len(self.uData)
+        return self.info[(self.championName, self.championID)]['VisionScore']
 
     def __eq__(self, championMainObj):
         return championMainObj.getChampionID() == self.getChampionID()
     
     def __str__(self):
         self.updateInfo()
-        KDAStr = str(self.info[self.championID]['KDA'][0]) + " / " + str(self.info[self.championID]['KDA'][1]) + " / " + str(self.info[self.championID]['KDA'][2])
-        WRStr = str(self.info[self.championID]['Winrate'] * 100)
-        CSStr = str(self.info[self.championID]['AvgCS'])
-        string = f"Player {self.player} has KDA {KDAStr} with an average win rate of {WRStr} and average CS/Min per game {CSStr} on champion {self.championName} with a total amount of games at {len(self.uData )}"
+        KDAStr = str(self.info[(self.championName, self.championID)]['KDA'][0]) + " / " + str(self.info[(self.championName, self.championID)]['KDA'][1]) + " / " + str(self.info[(self.championName, self.championID)]['KDA'][2])
+        WRStr = str(self.info[(self.championName, self.championID)]['Winrate'] * 100)
+        CSStr = str(self.info[(self.championName, self.championID)]['AvgCS'])
+        string = f"Player {self.player} has KDA {KDAStr} with an average win rate of {WRStr} and average CS/Min per game {CSStr} on champion {self.championName} with a total amount of games at {len(self.uData)}"
         return string
---
+++ Summoner.py
@@ -0,0 +1,110 @@
+import requests
+from info import info
+import pdb
+import json
+import time
+from Champion import ChampionMain
+
+class Summoner:
+    def __init__(self, name, tag):
+        self.matchDict = {}
+        self.summonerTag = (name, tag)
+        self.puuid = self.convertPuuid()
+
+    def convertPuuid(self):
+        url = f"https://{info['REGIONV1']}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{self.summonerTag[0]}/{self.summonerTag[1]}"
+        response = requests.get(url, headers=info['headers'])
+        response = json.loads(response.text)['puuid']
+        return response
+
+    def parseCurrentPatch(self, season, count):
+        # get match ids from count to count + 100
+        url = f"https://{info['REGIONV1']}.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.puuid}/ids?type=ranked&start={count}&count=100"
+        matches = json.loads(requests.get(url, headers=info['headers']).text)
+
+        # for each match id, add to a list
+        matchData = []
+        for match in matches:
+            time.sleep(1.2)
+            url = f"https://{info['REGIONV1']}.api.riotgames.com/lol/match/v5/matches/{match}"
+            response = json.loads(requests.get(url, headers=info['headers']).text)
+            # base case is when the season version does not start with the target season
+            #pdb.set_trace()
+            print(response['info']['gameVersion'])
+            if not response['info']['gameVersion'].startswith(season):
+                return matchData
+            # otherwise append to list and filter remade games
+            if not response['info']['gameDuration'] <= 360:
+                matchData.append(response)
+        # append lists from recursive method
+        matchData += self.parseCurrentPatch(self.puuid, season, count + 100)
+        # return the final list
+        return matchData
+
+    # precondition that this will be run nearly on the same level as init
+    def getAllMatches(self, season):
+        matches = self.parseCurrentPatch(season, 0)
+        self.matchDict['unparsedMatchInfo'] = matches
+
+    # parsing champion mains not acutlaly chapmion pool (untested as of 02/09/2025 03:00AM)
+    def parseChampionPool(self):
+        # list of championmain objects
+        self.championPoolObjects = []
+        data = self.matchDict['unparsedMatchInfo']
+        #pdb.set_trace()
+        for object in data:
+            for participant in object['info']['participants']:
+                if participant['puuid'] == self.puuid:
+                    tempObj = ChampionMain(participant)
+                    exists = False
+                    for championPoolObj in self.championPoolObjects:
+                        if tempObj == championPoolObj:
+                            championPoolObj.addStat(participant)
+                            exists = True
+                            break
+                    if not exists:
+                        self.championPoolObjects.append(tempObj)
+    
+    def getCObjects(self):
+        return self.championPoolObjects
+
+# response = url = f"https://{info['REGIONV1']}.api.riotgames.com/lol/match/v5/matches/{matches[-1]}"
+# response = json.loads(requests.get(url, headers=info['headers']).text)
+#ex match url https://americas.api.riotgames.com/lol/match/v5/matches/NA1_5157651823
+# match['metadata']['participants'] gets participants in puuid
+# match['info']['gameVersion'] gets match patch version
+# match['info']['gameDuration']
+# match['info']['participants'][num]['challenges']['acesBefore15Minutes']
+# match['info']['participants'][num]['riotIdGameName']
+# match['info']['participants'][num]['riotIdTagline']
+# match['info']['participants'][num]['spellf{num}Casts']
+# match['info']['participants'][num]['teamEarlySurrender'] = remake?
+# match['info']['participants'][num]['totalMinionsKilled']
+# match['info']['participants'][num]['visionScore']
+# match['info']['participants'][num]['goldEarned']
+# match['info']['participants'][num]['gameEndedInSurrender']
+# match['info']['participants'][num]['win'] done 
+# match['info']['participants'][num]['damageDealtToBuildings']
+# match['info']['participants'][num]['damageDealtToObjectives'] (participation in drag/baron is objectives-buildings)
+# match['info']['participants'][num]['championName']
+# match['info']['participants'][num]['kills'] done
+# match['info']['participants'][num]['deaths'] done
+# match['info']['participants'][num]['assists'] done
+# match['info']['participants'][num]['longestTimeSpentLiving']
+# match['info']['participants'][num]['totalDamageDealtToChampions']
+# match['info']['participants'][num]['totalHealsOnTeammates']
+# match['info']['participants'][num]['visionScore']
+# match['info']['participants'][num]['wardsKilled']
+# match['info']['participants'][num]['visionWardsBoughtInGame']
+# match['info']['participants'][num]['wardsPlaced']
+# match['info']['participants'][num]['challenges']['gameLength']
+# match['info']['participants'][num]['challenges']['goldPerMinute']
+# match['info']['participants'][num]['challenges']['maxCsAdvantageOnLaneOpponent']
+# match['info']['participants'][num]['challenges']['maxLevelLeadLaneOpponent']
+# match['info']['participants'][num]['firstBloodAssist']
+# match['info']['participants'][num]['firstBloodKill']
+# match['info']['participants'][num]['firstTowerAssist']
+# match['info']['participants'][num]['firstTowerKill']
+# match['info']['participants'][num]['challenges']['skillshotsDodged']
+# match['info']['participants'][num]['challenges']['soloKills']
+# match['info']['participants'][num]['challenges']["gameLength"]
